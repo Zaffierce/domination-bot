ede30caecbc75e4ad5edc2e3635f4cc44e090603
Jonathan Veach <veach09@gmail.com>
2019-05-23 14:34:41 +0900

    Rewrite to edit on fly.

--- commands/editserveradmin.js
+++ commands/editserveradmin.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 const serverAdmins = require('../data/serverAdmins.json');
 const fs = require("fs");
+const update = require('..update.json');
 
     module.exports.run = async (bot, message, args) => {
-        
+    
     let serverNumber = args[0];    
     let serverAdminList = message.content.split(' ').splice(2).join(' ');
     let modRole = message.member.hasPermission('KICK_MEMBERS');
@@ -11,19 +12,60 @@ const fs = require("fs");
         message.delete();
         console.log(message.author.username + " attempted to run a command that they don't have access to!");
         } else {
+            let channel = bot.channels.get(update.rules);
+            let announcements = bot.channels.get(update.announcements);
             if(!args[0] || !serverAdminList) { return message.channel.send("Please check your command and try again.\n**Example:** !beditserveradmin 1 Zaff, so this makes Zaff the only tribe on ISLAND.") }
             message.delete();
             if (!serverAdmins[serverNumber]) serverAdmins[serverNumber] = {
-                serverAdminList: ""
+                serverAdminList: "",
+                serverNum: "",
+                messageID: "none"
             };
+            let wserverAdminList = serverAdmins[serverNumber].serverAdminList;
             var newText = serverAdmins[serverNumber].serverAdminList = serverAdminList;
+            var newNum = serverAdmins[serverNumber].serverNum = serverNumber;
             newText;
-            message.channel.send("Attempting to edit Server #**"+serverNumber+"** admin list to the following... ");
-            message.channel.send("```"+serverAdminList+"```");
-            console.log(`${message.author.username} just edited server #${serverNumber} admin list.`);
-            fs.writeFile("data/serverAdmins.json", JSON.stringify(serverAdmins), (err) => {
-                if (err) console.log(err);
-            });
-
+            newNum;
+            
+            if (serverAdmins[serverNumber].messageID === "none") {
+                const embed = new Discord.RichEmbed()
+                    .setColor(`#F6DD0F`)
+                    .addField(`Ark Server #${serverNumber}`, `${serverAdminList}`);
+                message.channel.send("Adding new Server Admin list.  This will not appear until !postrules has been ran.");
+                message.channel.send(embed);
+                console.log(`${message.author.username} just edited server #${serverNumber} admin list.`);
+                fs.writeFile("data/serverAdmins.json", JSON.stringify(serverAdmins), (err) => {
+                    if (err) console.log(err);
+                });
+            } else {
+                const oldEmbed = new Discord.RichEmbed()
+                    .setColor(`#F6DD0F`)
+                    .addField(`Ark Server #${serverNumber}`,`${wserverAdminList}`);
+                announcements.send("A change has been made to the Server Admin List!");
+                announcements.send("**OLD LIST**");
+                announcements.send(oldEmbed).then(function() {
+                    var newText = serverAdmins[serverNumber].serverAdminList = serverAdminList;
+                    var newNum = serverAdmins[serverNumber].serverNum = serverNumber;
+                    newText;
+                    newNum;
+                    const embed = new Discord.RichEmbed()
+                        .setColor(`#F6DD0F`)
+                        .addField(`Ark Server #${serverNumber}`,`${wserverAdminList}`);
+                    let embedObjectID = Object.assign({}, embed.fields['0']);
+                    embedObjectID.value = serverAdminList;
+                    const newEmbed = new Discord.RichEmbed({
+                        color: embed.color,
+                        fields: [embedObjectID],
+                    });
+                    let serverListID = serverAdmins[serverNumber].messageID;
+                    channel.fetchMessage(serverListID).then(message => message.edit(newEmbed));
+                    announcements.send("**NEW LIST**");
+                    announcements.send(newEmbed);
+                });
+                console.log(`${message.author.username} just edited server #${serverNumber} admin list.`);
+                fs.writeFile("data/serverAdmins.json", JSON.stringify(serverAdmins), (err) => {
+                    if (err) console.log(err);
+                });
+            }
         }
     };
