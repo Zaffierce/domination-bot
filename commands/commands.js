85ff860b3337e94e4a7def14523f80b66c5da83c
Jonathan Veach <veach09@gmail.com>
2019-05-26 11:36:21 -0700

    Included breeder, emote, events and wiki

--- commands/commands.js
+++ commands/commands.js
@@ -6,6 +6,9 @@ module.exports.run = async (bot, message, args) => {
     const commandsEmbed = new Discord.RichEmbed()
         .setColor('#BABABA')
         .setAuthor('DomiNATION Bot commands list')
+        .addField(`!breeder`,`This provides information for Jezera's DomiNATION Breeding Project.`)
+        .addField(`!emote`,`Fun little emote command.  Type !emote for more information.`)
+        .addField(`!events`,`This provides the upcoming scheduled events for the DomiNATION Ark Servers.`)
         .addField(`!help`,`This provides information if you require assistance from the Admins.`)
         .addField(`!patreon`,`This provides the Patreon information.`)
         .addField(`!patrequest`,`This provides basic information for Ark's monthly Patreon dino claim.`)
@@ -13,13 +16,8 @@ module.exports.run = async (bot, message, args) => {
         .addField(`!rule [#]`,`This provides an Ark rule dependant to the number you provide: !rule 1 will display Ark Rule #1.`)
         .addField(`!website`,`This provides the DomiNATION website link.`)
         .addField(`!whitelist`,`This provides whitelisting steps for new Patreons.`)
-        // .addField(``,``)
-        // .addField(``,``)
-        // .addField(``,``)
-        // .addField(``,``)
+        .addField(`!wiki [query]`,`This searches the Ark Survival Evolved Wiki.`)
         .setTimestamp()
         .setFooter(`Command list last updated on:`);
-
-
-    message.channel.send(commandsEmbed)
+    message.channel.send(commandsEmbed);
     };
