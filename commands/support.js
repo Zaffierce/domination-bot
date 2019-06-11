06dc1a1b67d92b84d134c15c970d53e2b840ebb7
Jon Veach <veach09@gmail.com>
2019-06-11 11:28:18 -0700

    Updates to make things cleaner

--- commands/support.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-        
-    message.channel.send("Hello, your request is imporant to us.  Please fill out the following form so that an Admin may assist you in a timely manner.\nPlease state a time frame (I.E: 3-10PM EST) OR if this is a time sensitive matter, place ASAP in the blank.\nALL-IN-ONE REQUEST FORM - ELEMENT, HELP, PATREON DINO\nhttps://docs.google.com/forms/d/e/1FAIpQLSdSECTj0h464c-ic-SPoczURkGgnztY7YoE5ODWI2hDf6mDZQ/viewform"); }
-    
-    module.exports.config = {
-        command: "help"
-    }
