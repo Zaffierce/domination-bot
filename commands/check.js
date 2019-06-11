53869f9e698b07a31dd473f7d0d238f84e6441be
Jon Veach <veach09@gmail.com>
2019-06-11 10:13:53 -0700

    Removed loop, created embed and organized data

--- commands/check.js
+++ commands/check.js
@@ -1,7 +1,25 @@
+/*
+    To do:  [ ]Organize data better, maybe throw into field
+            [ ]Sum tickets
+*/
+
+const Discord = require("discord.js");
+const tickets = require("../data/tickets.json");
+
 module.exports.run = async (bot, message, args) => {
-    const tickets = require("../data/tickets.json");
-    message.delete();    
-    for(var i in tickets) {
-        myArray = tickets[i]
-        message.channel.send("["+i+"]" + " has closed ["+myArray.amount+"] tickets.")
-    }};
+        
+    const topTickets = Object.keys(tickets).sort(function(a,b){
+        return tickets[b].amount-tickets[a].amount
+    })
+    const mappedTopTickets = topTickets.map(c => `**${c}** - ${tickets[c].amount}`);
+    // console.log(mappedTop5.join("\n"));
+    message.delete();
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`Tickets closed by staff members...`)
+        .setDescription(mappedTopTickets)
+    // for(var i in tickets) {
+    //     myArray = tickets[i]
+    //     embed.addField(`${i}:`,`${myArray.amount}`, true)
+    // }
+    message.channel.send(embed);
+};
