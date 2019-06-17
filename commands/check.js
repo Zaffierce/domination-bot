1e88f73aea2669acf12318163ca63ce827c98703
Jon Veach <veach09@gmail.com>
2019-06-16 22:19:31 -0700

    Changed to DM

--- commands/check.js
+++ commands/check.js
@@ -12,14 +12,10 @@ module.exports.run = async (bot, message, args) => {
         return tickets[b].amount-tickets[a].amount
     })
     const mappedTopTickets = topTickets.map(c => `**${c}** - ${tickets[c].amount}`);
-    // console.log(mappedTop5.join("\n"));
     message.delete();
     const embed = new Discord.RichEmbed()
         .setAuthor(`Tickets closed by staff members...`)
         .setDescription(mappedTopTickets)
-    // for(var i in tickets) {
-    //     myArray = tickets[i]
-    //     embed.addField(`${i}:`,`${myArray.amount}`, true)
-    // }
-    message.channel.send(embed);
+    message.reply("please check your DM's").then(d_msg => { d_msg.delete(10000);});;
+    message.author.send(embed);
 };
