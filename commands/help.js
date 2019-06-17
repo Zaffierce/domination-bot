8e3c6661738bd5c0cf77730b2950205800950a3d
Jon Veach <veach09@gmail.com>
2019-06-16 21:32:18 -0700

    Changed to embed to shorten length of message

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,16 @@
+const Discord = require("discord.js");
+
+//Create embed help command?  
+//Create universal help command based on where the command is ran?  Talk to Alphy.
+
 module.exports.run = async (bot, message, args) => {
-        
-    message.channel.send("Hello, your request is important to us.  Please fill out the form below and an Admin will contact you when available.  Please be aware that tickets take upwards of 5 minutes to appear in our queue, so please be patient!\nhttps://goo.gl/forms/ScVJJxXLA53HTCsx2"); }
+
+    const embed = new Discord.RichEmbed()
+    .setAuthor(`DomiNATION Support Form`)
+    .setDescription(`Hello, your request is important to us.  Please fill out the form below and an Admin will contact you when available.  Please be aware that tickets take upwards of 5 minutes to appear in our queue, so please be patient.\n\nhttps://goo.gl/forms/ScVJJxXLA53HTCsx2`)
+message.channel.send(embed);
+
+}
     
     module.exports.config = {
         command: "help"
