600f050f918316c3c8f4f2618e8dad624a5bbf71
Jon Veach <veach09@gmail.com>
2019-06-30 12:50:20 -0700

    Added event map link to embed

--- commands/events.js
+++ commands/events.js
@@ -10,6 +10,7 @@ const events = require("../data/events.json");
             .setColor(`#42ebf4`)
             .setDescription(`${eventText}`)
             .setThumbnail('https://cdn.discordapp.com/attachments/566853064967847946/581337829628444682/download.jpg')
+            .addField(`Event Map download link: `,`https://steamcommunity.com/sharedfiles/filedetails/?id=788341599`)
             .addField(`If you need to use a Time Zone Converter, please visit this link.  Note:  This automatically includes the EDT/EST Time Zone.`,`https://www.timeanddate.com/worldclock/converter.html?&p1=179`)
         message.reply("please check your DM's").then(d_msg => { d_msg.delete(60000);});;
         message.author.send(embed);
