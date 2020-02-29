9cd109e6108b9c62739107e8519ecdeaea2fa48e
Zaffierce <veach09@gmail.com>
2020-02-29 15:22:04 -0800

    Changed support link

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
             const embed = new Discord.RichEmbed()
                 .setAuthor(`DomiNATION Ark Support Form`)
                 .setThumbnail('http://orig14.deviantart.net/2c5f/f/2015/311/3/5/ark_survival_evolved_icon_by_troublem4ker-d9fw57a.png')
-                .setDescription(`Hello, your request is important to us.  Please fill out the form below and an Admin will contact you when available.  Please be aware that tickets take upwards of 5 minutes to appear in our queue, so please be patient.\n\nhttps://goo.gl/forms/ScVJJxXLA53HTCsx2`)
+                .setDescription(`Hello, your request is important to us.  Please fill out the form below and an Admin will contact you when available.  Please be aware that this is a new system that is still in development, so please be patient.\n\nhttps://support.domination-gaming.com/`)
             message.channel.send(embed);
         }
         if (message.channel.id === update.atlasSupport) {
