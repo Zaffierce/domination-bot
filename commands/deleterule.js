76999fed9e9548536b78c545396191ea4509a355
Zaffierce <veach09@gmail.com>
2020-04-09 15:35:42 -0700

    Cleanup & changed to switch functions

--- commands/deleterule.js
+++ commands/deleterule.js
@@ -1,63 +1,61 @@
-const Discord = require("discord.js");
 const arkRules = require('../data/arkRules.json');
 const discordRules = require('../data/discordRules.json');
 const patreonRules = require('../data/patreonRules.json');
 const fs = require("fs");
 
-    module.exports.run = async (bot, message, args) => {
-        
-    let ruleType = args[0];
-    let ruleNumber = args[1];    
-    let ruleText = message.content.split(' ').splice(2).join(' ');
-    let modRole = message.member.hasPermission('KICK_MEMBERS');
-    if(!modRole) {     
-        message.delete();
-        console.log(message.author.username + " attempted to run a command that they don't have access to!");
+module.exports.run = async (bot, message, args) => {
+      
+  let ruleType = args[0];
+  let ruleNumber = args[1];    
+  let modRole = message.member.hasPermission('KICK_MEMBERS');
+
+  if(!modRole) {     
+      message.delete();
+  } else {
+    if(!args[0] || !args[1]) return message.channel.send("Please check your command and try again.\n**Example:** `!bdeleterule ark 18` - This will delete Ark Rule #18.");
+
+    switch(ruleType) {
+      case 'ark':
+        if (!arkRules[ruleNumber]) {
+          message.channel.send(`I am unable to find Ark Rule #${ruleNumber}, please check again.`);
         } else {
-            if(!args[0] || !args[1]) { return message.channel.send("Please check your command and try again.\n**Example:** !bdeleterule ark 18 .... This will delete Ark Rule #18.");}
+          message.channel.send(`I am now deleting Ark Rule #${ruleNumber}.`);
+          message.channel.send("```"+arkRules[ruleNumber].ruleText+"```");
+          delete arkRules[ruleNumber].ruleText;
+          delete arkRules[ruleNumber];
+          fs.writeFile("data/arkRules.json", JSON.stringify(arkRules), (err) => {
+            if (err) console.log(err);
+          });
+        }
+        break;
 
+      case 'discord':
+        if (!discordRules[ruleNumber]) {
+          message.channel.send(`I am unable to find Discord Rule #${ruleNumber}, please check again.`);
+        } else {
+          message.channel.send(`I am now deleting Discord Rule #${ruleNumber}.`);
+          message.channel.send("```"+discordRules[ruleNumber].ruleText+"```");
+          delete discordRules[ruleNumber].ruleText;
+          delete discordRules[ruleNumber];
+          fs.writeFile("data/discordRules.json", JSON.stringify(discordRules), (err) => {
+            if (err) console.log(err);
+          });
+        }
+        break;
 
-            if(ruleType === "ark") {
-                if (!arkRules[ruleNumber]) {
-                    message.channel.send("Unable to find Rule #**" +ruleNumber+"**, please check again.")
-                } else {
-                message.channel.send("Attempting to delete Rule #**" +ruleNumber+"**, it used to state...");
-                message.channel.send("```"+arkRules[ruleNumber].ruleText+"```");
-                delete arkRules[ruleNumber].ruleText;
-                delete arkRules[ruleNumber];
-                console.log(`${message.author.username} just deleted Rule#${ruleNumber}.`);
-                fs.writeFile("data/arkRules.json", JSON.stringify(arkRules), (err) => {
-                    if (err) console.log(err);
-                });
-            }}
-            if(ruleType === "discord") {
-                if (!discordRules[ruleNumber]) {
-                    message.channel.send("Unable to find Rule #**" +ruleNumber+"**, please check again.")
-                } else {
-                message.channel.send("Attempting to delete Rule #**" +ruleNumber+"**, it used to state...");
-                message.channel.send("```"+discordRules[ruleNumber].ruleText+"```");
-                delete discordRules[ruleNumber].ruleText;
-                delete discordRules[ruleNumber];
-                console.log(`${message.author.username} just deleted Rule#${ruleNumber}.`);
-                fs.writeFile("data/discordRules.json", JSON.stringify(discordRules), (err) => {
-                    if (err) console.log(err);
-                });
-            }}
-            if(ruleType === "patreon") {
-                if (!patreonRules[ruleNumber]) {
-                    message.channel.send("Unable to find Rule #**" +ruleNumber+"**, please check again.")
-                } else {
-                message.channel.send("Attempting to delete Rule #**" +ruleNumber+"**, it used to state...");
-                message.channel.send("```"+patreonRules[ruleNumber].ruleText+"```");
-                delete patreonRules[ruleNumber].ruleText;
-                delete patreonRules[ruleNumber];
-                console.log(`${message.author.username} just deleted Rule#${ruleNumber}.`);
-                fs.writeFile("data/patreonRules.json", JSON.stringify(patreonRules), (err) => {
-                    if (err) console.log(err);
-                });
-            }}
-
-
-
-            }
-        };
+      case 'patreon':
+        if (!patreonRules[ruleNumber]) {
+          message.channel.send(`I am unable to find Patreon Rule #${ruleNumber}, please check again.`);
+        } else {
+          message.channel.send(`I am now deleting Discord Rule #${ruleNumber}.`);
+          message.channel.send("```"+patreonRules[ruleNumber].ruleText+"```");
+          delete patreonRules[ruleNumber].ruleText;
+          delete patreonRules[ruleNumber];
+          fs.writeFile("data/patreonRules.json", JSON.stringify(patreonRules), (err) => {
+            if (err) console.log(err);
+          });
+        }
+        break;
+    }
+  }
+};
