ac26fd426d9e941e695cc06890a246e5747a0769
Zaffierce <veach09@gmail.com>
2020-04-13 17:02:17 -0700

    Changed to DM

--- commands/patrequest.js
+++ commands/patrequest.js
@@ -1,5 +1,18 @@
-module.exports.run = async (bot, message, args) => {
-        
-    message.channel.send("*Please note that it takes a few days for us to receive the Patreon list, so please be patient at the beginning of each month.*\n\n**How to request**: https://goo.gl/forms/ScVJJxXLA53HTCsx2\n\n**What you can request**:\n   - *Any dino that spawns **NATURALLY** within Ark, at the max level available of 225 for regular dinos and 270 for Tek.*\n\n**What you cannot request**:\n   - *A DodoRex at level 1000*\n   - *Specific stats on your dino*\n\n**Other information**:\n   - *If you are a $10 supporter, you have insurance on your spawned dino.  This means if this dino dies for some reason, we will replace it once and only once!*\n   - *You can choose to have the colors changed on the dino that we spawn, however, do note that this will cause it to be spayed/neutered*\n\n");
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+  let embed = new Discord.MessageEmbed()
+    .setColor('#8500FF')
+    .setTitle('Patreon Request')
+    .setURL('https://support.domination-gaming.com/')
+    .setThumbnail('https://cdn.discordapp.com/attachments/480355364613783566/570192827699560458/pirate-dilo-2_1.png')
+    .setDescription('**Please note that it takes a few days for us to receive the Patreon list, so please be patient and wait for us to announce in #announcements-supporters channel before submitting a request.**')
+    .addFields(
+      {name: '**How do I request my dinosaur?**', value:"Click the link here - https://support.domination-gaming.com/ - and choose the '**Patreon Request**' option in the dropdown", inline: false},
+      {name: '**What can I request?**', value:'You may request any dinosaur that spawns **NATURALLY** within the chosen Ark, at the max level available for that type.  You may also choose the gender and color, but note that if you do choose color then that dinosaur will be neutered.', inline: false},
+      {name: "**What's the limitations?**", value:'You may not request specific levels on your dinosaur, whatever rolls when we spawn it is what you get.', inline: false},
+      {name: '**Other information**', value:'If you are a $10+ supporter, you have insurance on the requested dinosaur. If something unfortunate were to happen to it, you are able to submit a ticket to get a replacement.', inline: false}
+    )    
+  message.reply("please check your DM's").then(d_msg => { d_msg.delete({timeout:60000});});;
+  message.author.send(embed);
 };
