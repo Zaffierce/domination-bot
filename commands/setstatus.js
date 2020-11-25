d83510e4a815c167fa39f35a974e0b9d8c42c3d0
Jon Veach <ozzy_ozbourn@hotmail.com>
2020-11-25 11:46:17 -0500

    Corrected lowercasing

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,11 +1,12 @@
 module.exports.run = async (bot, message, args) => {
-  
+
   let modRole = message.member.hasPermission('KICK_MEMBERS');
   
   if(!modRole) {     
     message.delete();
   } else {
-    const status = args.join(' ');
+    const status = message.content.split(' ').splice(1).join(' ');
+    console.log(status);
     bot.user.setActivity(status);
   }
 };
